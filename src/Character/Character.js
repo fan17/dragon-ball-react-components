855a5f6f1ba4345fe895149f048193c5e1c93e30
Andrzej Jędrzejczak <jedrzejczak.andrzej89@gmail.com>
2019-03-04 19:52:51 +0100

    install storybook

---
+++ src/Character/Character.js
@@ -0,0 +1,7 @@
+import React, { PureComponent } from "react";
+
+export default class CharacterComponent extends PureComponent {
+  render() {
+    return <div>example CharacterComponent</div>;
+  }
+}
---
+++ src/Character/__stories__/Character.stories.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+import { storiesOf } from "@storybook/react";
+import Character from "../Character";
+
+storiesOf("Character", module).add("default", () => <Character />);
